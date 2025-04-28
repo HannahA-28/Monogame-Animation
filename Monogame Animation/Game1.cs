28d0eb56308ed1215773d00ea8cd1a8ae1958181
Hannah Anderson <339729345@GoTVDSB.ca>
2025-04-28 14:36:26 -0400

    Add project files.

---
+++ Monogame Animation/Game1.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Monogame_Animation
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        Rectangle window;
+
+        Texture2D greyHairtexture;
+        Rectangle greyHairRect;
+        Vector2 greyHairSpeed;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            window = new Rectangle(0, 0, 800, 600);
+            _graphics.PreferredBackBufferWidth = window.Width;
+            _graphics.PreferredBackBufferHeight = window.Height;
+            _graphics.ApplyChanges();
+
+            greyHairRect = new Rectangle(300, 10, 100, 100);
+            greyHairSpeed = new Vector2(2, 2);
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+
+            greyHairtexture = Content.Load<Texture2D>("greyHair");
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            greyHairRect.X += (int)greyHairSpeed.X;
+            if (greyHairRect.Right >= window.Width || greyHairRect.Left <= 0)
+            {
+                greyHairSpeed.X *= -1;
+            }
+            if (greyHairRect.Bottom >= window.Height || greyHairRect.Top <= 0)
+            {
+                greyHairSpeed.Y *= -1;
+            }
+            greyHairRect.Y += (int)greyHairSpeed.Y;
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+
+            _spriteBatch.Begin();
+
+            _spriteBatch.Draw(greyHairtexture, greyHairRect, Color.White);
+
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
